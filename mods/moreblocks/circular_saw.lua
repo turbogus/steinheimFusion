b54188fac23997a1dcb5cd2b3c806a3b4f401975
turbogus <turbogus85@gmail.com>
2014-12-19 21:39:14 +0100

    Circularsaw : uniformisation du formspec avec les formspecs défaut.

--- mods/moreblocks/circular_saw.lua
+++ mods/moreblocks/circular_saw.lua
@@ -279,6 +279,11 @@ function circular_saw.on_construct(pos)
 			"field[0.3,3.5;1,1;max_offered;" ..S("Max").. ":;${max_offered}]" ..
 			"button[1,3.2;1,1;Set;" ..S("Set").. "]" ..
 			"list[current_name;output;2.8,0;8,4;]" ..
+			
+			"bgcolor[#080808BB;true]"..
+			"background[5,5;1,1;circularsaw_bg.png;true]"..		-- ajouter par turbogus
+			"listcolors[#00000069;#5A5A5A;#141318;#30434C;#FFF]"..	-- ajouter par turbogus
+			
 			"list[current_player;main;1.5,5;8,4;]")
 
 	meta:set_int("anz", 0) -- No microblocks inside yet.
